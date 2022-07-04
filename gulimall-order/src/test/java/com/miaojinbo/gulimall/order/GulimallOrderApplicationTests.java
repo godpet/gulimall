b05836f3ee7041bd3eeb5588def23cdf955d84f1
苗金波 <miao122015@163.com>
2022-07-04 09:41:50 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/miaojinbo/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaojinbo.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
